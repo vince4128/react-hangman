947fb2fc270c47ece4d3f84cc6ad981a09948789
vince4128 <vrlt.vincent@gmail.com>
2019-08-05 14:37:15 +0200

    random word added

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {randomWord} from '../words';
 import "./Hangman.css";
 
 import img0 from "../0.jpg";
@@ -18,7 +19,7 @@ class Hangman extends Component {
 
     constructor(props){
         super(props);
-        this.state = { nWrong:0, guessed: new Set(), answer:"apple"};
+        this.state = { nWrong:0, guessed: new Set(), answer:randomWord()};
         this.handleGuess = this.handleGuess.bind(this);
     }
 
