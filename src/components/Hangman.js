07103c9e5f121dfce3b1d938eefe87e5bfedf00f
vince4128 <vrlt.vincent@gmail.com>
2019-08-05 11:10:44 +0200

    guessedWord method added

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -3,11 +3,27 @@ import "./Hangman.css";
 
 class Hangman extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = { nWrong:0, guessed: new Set(), answer:"apple"};
+        //this.handleGuess = this.handleGuess.bind(this);
+    }
+
+    /**
+     guessedWord : return current state of word :
+     ex : return " a p p _ _ " for "apple" (if a and p have been guessed)
+    */
+    guessedWord() {
+        return this.state.answer
+            .split("")
+            .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"))
+    }
+
     render() {
         return (
             <div className='Hangman'>
                 <h1>Hangman</h1>                
-                <p>guessedWord</p>
+                <p className='Hangman-word'>{this.guessedWord()}</p>
                 <p>Letter button</p>
             </div>
         )
